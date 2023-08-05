f4e8c1346c4fd182c3e8ccf1ccfa051d985f05ab
Surendra Kumar <9038032+bhatiasurendra1@users.noreply.github.com>
2023-08-05 21:09:43 +0530

    Create EvenSubarrays.java

---
+++ 03. CarryForward/EvenSubarrays.java
@@ -0,0 +1,26 @@
+/***
+Even Subarrays
+
+Problem Description
+
+You are given an integer array A.
+Decide whether it is possible to divide the array into one or more subarrays of even length such that the first and last element of all subarrays will be even.
+Return "YES" if it is possible; otherwise, return "NO" (without quotes).
+
+Problem Constraints
+1 <= |A|, A[i] <= 106
+
+Input Format
+The first and the only input argument is an integer array, A.
+
+Output Format
+Return a string "YES" or "NO" denoting the answer.
+***/
+
+public class EvenSubarrays {
+    public String findEvenSubarrays(int[] A) {
+        if(A.length % 2 != 0) return "NO";
+        if(A[0]%2 == 0 && A[A.length - 1]%2 == 0) return "YES";
+        return "NO";
+    }
+}
